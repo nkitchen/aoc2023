22a73c950168560f04f01d83a643556e17b7b0d5
Nathan Kitchen <nathan.kitchen@gmail.com>
2024-08-26 19:10:27 -0700

    Day 17, part 1, second attempt
    Try distinguishing states by velocity and run length, not just
    location.
    
    Maybe it would get a correct result, but it's much too slow.

--- day17.py
+++ day17.py
@@ -5,7 +5,8 @@ import os
 from pprint import pprint
 
 import heapq
-from collections import namedtuple
+import math
+from collections import namedtuple, defaultdict
 
 DEBUG = int(os.environ.get("DEBUG", "0"))
 
@@ -13,7 +14,7 @@ def dprint(*args):
     if DEBUG:
         print(*args)
 
-State = namedtuple('State', 'est_loss loss loc vel run pred')
+State = namedtuple('State', 'loc vel run')
 
 def rot_left(vel):
     return (-vel[1], vel[0])
@@ -84,45 +85,59 @@ def main():
             if (0 <= ii < m) and (0 <= jj < n):
                 yield vv, (ii, jj)
 
-    visited = {}
     pred = {}
-    q = [State(min_loss_to_factory[(0, 0)], 0, (0, 0), (0, 0), 0, None)]
+    start = State(loc=(0, 0), vel=(0, 0), run=0)
+    loss_so_far = defaultdict(lambda: math.inf)
+    loss_so_far[start] = 0
+    q = [(min_loss_to_factory[start.loc], start)]
+    frontier = set([start])
     while q:
-        s = heapq.heappop(q)
-        if s.loss < visited.get(s.loc, s.loss + 1):
-            visited[s.loc] = s.loss
-        else:
-            continue
+        e, s = heapq.heappop(q)
+        if s.loc == (m - 1, n - 1):
+            break
 
-        pred[s.loc] = s.pred
+        frontier.remove(s)
+        #if s.loss < visited.get(s.loc, s.loss + 1):
+        #    visited[s.loc] = s.loss
+        #else:
+        #    continue
+
+        #pred[s.loc] = s.pred
 
         if DEBUG & 4:
             for i in range(m):
                 for j in range(n):
-                    v = str(visited.get((i, j), "."))
+                    v = str(loss_so_far.get(State(loc=(i, j)), "."))
                     ml = min_loss_to_factory[(i, j)]
                     print(f"{grid[i][j]:>4}:{v:>2}+{ml:>2} ", end='')
                 print()
             print()
 
-        dprint("==", s)
-
-        if s.loc == (m - 1, n - 1):
-            break
+        dprint("==", e, s)
 
         for vel, dst in moves(s):
             if vel == s.vel:
                 run = s.run + 1
             else:
                 run = 1
+            ss = State(loc=dst, vel=vel, run=run)
             ii, jj = dst
-            loss = s.loss + grid[ii][jj]
-            if loss > visited.get(dst, loss + 1):
-                continue
-            est_loss = loss # + min_loss_to_factory[(ii, jj)]
-            ss = State(est_loss, loss, dst, vel, run, s.loc)
-            dprint("  > ", ss)
-            heapq.heappush(q, ss)
+            loss = loss_so_far[s] + grid[ii][jj]
+            if loss < loss_so_far[ss]:
+                pred[dst] = s.loc
+                loss_so_far[ss] = loss
+                est_loss = loss + min_loss_to_factory[dst]
+                if ss not in frontier:
+                    frontier.add(ss)
+                    heapq.heappush(q, (est_loss, ss))
+                    dprint("  > ", est_loss, ss)
+
+            ##if loss > visited.get(dst, loss + 1):
+            ##    continue
+            #est_loss = loss # + min_loss_to_factory[(ii, jj)]
+            #ss = State(est_loss, loss, dst, vel, run, s.loc)
+            #dprint("  > ", ss)
+            #heapq.heappush(q, ss)
 
     path = [["."] * n for i in range(m)]
     loc = (m - 1, n - 1)
