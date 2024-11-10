c8fbefb9be39ea25b3391a2a22c9c317e0879842
Nathan Kitchen <nathan.kitchen@gmail.com>
2024-11-09 21:09:46 -0800

    Day 23, part 2, attempt 1
    Naive greedy approach: Always take the largest-weight edge available.
    Doesn't find a path.

--- day23.py
+++ day23.py
@@ -5,6 +5,7 @@ import os
 import sys
 from pprint import pprint
 
+import functools
 import heapq
 from collections import defaultdict
 from collections import namedtuple
@@ -16,78 +17,126 @@ def dprint(*args):
         print(*args)
 
 def main():
-    grid = [list(line) for line in fileinput.input()]
+    grid = [list(line.rstrip()) for line in fileinput.input()]
     M = len(grid)
     N = len(grid[0])
 
+    assert grid[0][1] == '.'
+    assert grid[M - 1][N - 2] == '.'
+    start = (0, 1)
+    end = (M - 1, N - 2)
+
     def _adj(i, j):
-        if i > 0 and grid[i - 1][j] != '#':
+        if i > 0 and grid[i - 1][j] != '#' and grid[i][j] in '.^':
             yield (i - 1, j)
-        if i < M - 1 and grid[i + 1][j] != '#':
+        if i < M - 1 and grid[i + 1][j] != '#' and grid[i][j] in '.v':
             yield (i + 1, j)
-        if j > 0 and grid[i][j - 1] != '#':
+        if j > 0 and grid[i][j - 1] != '#' and grid[i][j] in '.<':
             yield (i, j - 1)
-        if j < N - 1 and grid[i][j + 1] != '#':
+        if j < N - 1 and grid[i][j + 1] != '#' and grid[i][j] in '.>':
             yield (i, j + 1)
 
-    # Do the trails form a DAG?
-
-    rep = {}
-    def _lookup_rep(loc):
-        r = rep.get(loc, loc)
-        if r != loc:
-            rep[loc] = r = _lookup_rep(r)
-        return r
-
-    def _join(loc1, loc2):
-        r1 = _lookup_rep(loc1)
-        r2 = _lookup_rep(loc2)
-        if r1 != r2:
-            r1, r2 = (min(r1, r2), max(r1, r2))
-            rep[r2] = r1
-
-    # Find connected regions.
+    # The slopes are always arranged in "gadgets" surrounding single "nexus"
+    # tiles.
+    # First step: Find the nexuses.
+    nexuses = set()
     for i in range(M):
         for j in range(N):
+            assert grid[i][j] not in '^<'
+
             if grid[i][j] != '.':
                 continue
+            if not (0 < i < M - 1):
+                continue
+            if not (0 < j < N - 1):
+                continue
+            if grid[i - 1][j] == grid[i + 1][j] == 'v':
+                nexuses.add((i, j))
+            elif grid[i][j- 1] == grid[i][j + 1] == '>':
+                nexuses.add((i, j))
+    
+    # Construct an acyclic graph connecting:
+    # - the start point
+    # - the end point
+    # - the nexus points
+    # Each edge is labeled with the number of steps between the points it
+    # connects.
+    steps_from = defaultdict(list)
+    search_starts = [start]
+    while search_starts:
+        u = search_starts.pop(0)
 
-            for ii, jj in _adj(i, j):
-                if grid[ii][jj] == '.':
-                    _join((i, j), (ii, jj))
-    # Settle reps.
-    for i in range(M):
-        for j in range(N):
-            _lookup_rep((i, j))
+        steps = {u: 0}
+        q = [u]
+        while q:
+            v = q.pop(0)
+            for w in _adj(*v):
+                if w in steps:
+                    continue
+                steps[w] = 1 + steps[v]
+                if w in nexuses:
+                    steps_from[u].append((w, steps[w]))
+                    if w not in steps_from and w not in search_starts:
+                        search_starts.append(w)
+                elif w == end:
+                    steps_from[u].append((w, steps[w]))
+                else:
+                    q.append(w)
 
-    # Collect connections between regions.
-    slope_edges = set()
-    for i in range(M):
-        for j in range(N):
-            if grid[i][j] == '^':
-                u = _lookup_rep((i + 1, j))
-                v = _lookup_rep((i - 1, j))
-                slope_edges.add((u, v))
-            elif grid[i][j] == 'v':
-                u = _lookup_rep((i - 1, j))
-                v = _lookup_rep((i + 1, j))
-                slope_edges.add((u, v))
-            elif grid[i][j] == '<':
-                u = _lookup_rep((i, j + 1))
-                v = _lookup_rep((i, j - 1))
-                slope_edges.add((u, v))
-            elif grid[i][j] == '>':
-                u = _lookup_rep((i, j - 1))
-                v = _lookup_rep((i, j + 1))
-                slope_edges.add((u, v))
+    if (graph_file := os.environ.get("GRAPH")):
+        with open(graph_file, "w") as f:
+            def gpr(s):
+                print(s, file=f)
 
-    # Emit graph.
-    print("digraph {")
-    for u, v in slope_edges:
-        i, j = u
-        ii, jj = v
-        print(f"  x{i}_{j} -> x{ii}_{jj};")
-    print("}")
+            gpr("digraph {")
+            for u in steps_from:
+                i, j = u
+                for v, d in steps_from[u]:
+                    ii, jj = v
+                    gpr(f'  x{i}_{j} -> x{ii}_{jj} [label="{d}"];')
+            gpr("}")
+
+    @functools.lru_cache(maxsize=None)
+    def _max_steps_to_end_from(v):
+        if v == end:
+            return 0
+
+        return max(d + _max_steps_to_end_from(w)
+                   for w, d in steps_from[v])
+
+    hike = _max_steps_to_end_from(start)
+    print("Part 1:", hike)
+
+    # Part 2: I can still use the graph between nexus points, but I need to
+    # treat the edges as reversible.
+
+    # Add the reverse edges.
+    adj = steps_from.copy()
+    for u in steps_from:
+        for v, d in steps_from[u]:
+            adj[v].append((u, d))
+
+    # But: The longest-path problem is NP-complete in general.
+    # I'm going to take a blind leap of faith, and trust that I can get the
+    # right path by a naive greedy approach: At each step, take the largest-weight
+    # edge available (that is, that goes to an unvisited point).
+
+    visited = set(start)
+    u = start
+    hike = 0
+    while True:
+        if u == end:
+            break
+
+        choices = [(d, v) for v, d in steps_from[u]
+                   if v not in visited]
+        choices.sort()
+        d, v = choices[-1]
+        visited.add(v)
+        hike += d
+        u = v
+
+    print("Part 2:", hike)
 
 main()
 
